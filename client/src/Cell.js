b675e0ceb61b686fde2be9422a2f39887f77183c
bond95 <cyberbond95@gmail.com>
2020-04-16 01:46:07 +0200

    Added player color

--- client/src/Cell.js
+++ client/src/Cell.js
@@ -1,14 +1,18 @@
-import { Bitmap } from '@createjs/easeljs'
+import { Bitmap, ColorFilter } from '@createjs/easeljs'
 
 export const GRASS_TEXTURE = '/images/grass.jpg'
 
 export default class Cell {
-  constructor(stage, texture, callback) {
+  constructor(stage, texture, position, callback) {
     this.stage = stage
     this.texture = texture
     this.extention = null
+    this.owner = null
+    this.position = position
     this.obj = new Bitmap(texture)
     this.obj.addEventListener('click', callback)
+    this.obj.x = position.x * 50
+    this.obj.y = position.y * 50
   }
   getTexture = () => {
     return this.texture
@@ -23,8 +27,13 @@ export default class Cell {
   getExtention = () => {
     return this.extention
   }
-  setPositionToRender = ({ x, y }) => {
-    this.obj.x = x
-    this.obj.y = y
+  getPosition = () => this.position
+  setOwner = (owner) => {
+    this.owner = owner
+
+    // Adding owner color to cell
+    const filter = new ColorFilter(1, 1, 1, 1, owner.getColor()[0], owner.getColor()[1], owner.getColor()[2]);
+    this.obj.filters = [filter];
+    this.obj.cache(0, 0, this.obj.getBounds().width, this.obj.getBounds().height)
   }
 }
